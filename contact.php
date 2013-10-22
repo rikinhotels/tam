1da3b844286ec8f3478ea9a13cc28d197bc2e9e8
Aditya Gada <aditya@slmbk.com>
2013-10-22 15:12:28 +0530

    tamm

---
+++ contact.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Contact Us | Tamanna Foundation NGO</title>
+		<link rel="icon" type="image/ico" href="images/favicon.ico" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="author" content="Overclockers Design" />
+		<meta name="keywords" content="Tamanna,TAMANNA,Tamanna Foundation,TAMANNA FOUNDATION,Tamanna Foundation NGO,TAMANNA FOUNDATION NGO,Tamanna Ngo,TAMANNA NGO,Tamanna Ngo Mumbai,TAMANNA NGO Mumbai,NGO Mumbai" />
+		<meta name="description" content="We are TAMANNA FOUNDATION , the pioneering, national and non-profit organization, established on 30th June, 2010. The Tamanna Foundation has been fully dedicated to the cause of improving the lives of the differently abled children and has also achieved immense success in their venture. We provide support and services to physically & mentally challenged Children. We provide disabled children with assistance to obtain wheelchairs, orthopedic braces, walker, lifts, hearing aids, eyeglasses, medical equipment, physical therapy, and surgery. Blind, Deaf, Amputees, and children with Cerebral Palsy, Muscular Dystrophy, Spastic Quadriplegia, Encephalitis, Rheumatoid Arthritis, Spina Bifida, Down's Syndrome, and other disabilities receive assistance. Tamanna Foundation focuses special attention on helping children." lang="en" />
+		<meta name="abstract" content="Tamanna Foundation NGO" />
+		<meta name="distribution" content="Global" />
+		
+		<link rel="Stylesheet" type="text/css" href="css/style1.css"></link>
+		<link rel="stylesheet" href="themes/default/default.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+		
+		<script type="text/javascript" src="js/jquery-1.9.0.min.js"></script>
+		<script type="text/javascript" src="js/jquery.nivo.slider.js"></script>
+		<script type="text/javascript">
+			$(window).load(function() {
+				$('#slider').nivoSlider();
+			});
+		</script>
+		
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-39939312-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
+	</head>
+	<body style="background-image:url('images/tamannangocontactbackground.jpg');">
+		<div class="wrapper">
+			<div class="header">
+				<div class="logo">
+					<a href="index.html"><img src="images/tamannangologo.png" alt="Tamanna Foundation NGO Logo" title="Tamanna Foundation NGO"/></a>
+				</div>
+				<div class="menu">
+					<ul>
+						<li><a href="index.html">HOME</a></li>
+						<li><a href="about.html">ABOUT</a></li>
+						<li><a href="mission.html">MISSION</a></li>
+						<li><a href="events.html">EVENTS</a></li>
+						<li><a href="supporters.html">SUPPORTERS</a></li>
+						<li class="active">CONTACT</li>
+					</ul>	
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="content">
+				<div class="sliderback">
+					<div id="wrapper">
+        
+
+						<div class="slider-wrapper theme-default">
+							<div id="slider" class="nivoSlider">
+								<img src="images/slider/1.jpg" data-thumb="images/slider/1.jpg" style="width:720px;" alt="" />
+								<img src="images/slider/2.jpg" data-thumb="images/slider/2.jpg" style="width:720px;" alt=""/>
+								<img src="images/slider/3.jpg" data-thumb="images/slider/3.jpg" style="width:720px;" alt=""/>
+								<img src="images/slider/4.jpg" data-thumb="images/slider/4.jpg" style="width:720px;" alt=""/>
+							</div>
+						</div>
+
+					</div>		
+				</div>
+				<div class="quote">
+					<p>
+						“ Enable the Disabled; Translate Disability into Ability;<br/>
+						Capability, a winning Opportunity-Indeed a Reality ”
+					</p>
+					<h2>- Dr Veena Kumari</h2>	
+				</div>
+			</div>
+		</div>
+			
+					<div class="abouttext">
+						<h2>Contact Us</h2>
+						
+						<div class="aleft">
+						
+							<h3>TAMANNA FOUNDATION</h3>
+							<p>
+								603, Stardeep Appt.<br/>
+								Opp. Prabhodhan Thackrey Hall,<br/>
+								Sodawala Lane, Boriwali (W),<br/>
+								Mumbai - 400092, India
+							</p>	
+							<h3>Directors:</h3>
+							<p style="font-size:14px;">
+								Amita Jain - 9819964588<br/>
+								Erina A. Jain - 9869340416
+							</p>
+							<h3>Helping Hands:</h3>
+							<table cellspacing="0" cellpadding="0">
+								<tr>
+									<th id="tdh">
+										Place:
+									</th>
+									<th id="tdh">
+										Cordinators
+									</th>
+								</tr>
+								<tr>	
+									<td id="tdh" style="border-right:0px;border-bottom:0px;">
+										South Mumbai
+									</td>
+									<td id="tdh" style="border-bottom:0px;">
+										Vinita Navroz - 9821186772
+									</td>					
+								</tr>
+								<tr>
+									<td rowspan="2" id="tdh" style="border-right:0px;border-bottom:0px;">
+										Vile Parle
+									</td>
+									<td id="tdh" style="border-bottom:0px;">
+										Preksha Shah - 9820575894
+									</td>
+								</tr>
+								<tr>	
+									<td id="tdh" style="border-bottom:0px;">
+										Roma Shah - 9004010795
+									</td>
+								</tr>
+								<tr>
+									<td id="tdh" style="border-right:0px;">
+										Borivali
+									</td>
+									<td id="tdh">
+										Lalita Jain - 9820250867
+									</td>
+								</tr>	
+							</table>
+						<h3>To view full list of volunteers <a href="volunteers.pdf" target="_blank">click here</a></h3>
+						</div>
+						<div class="aright">
+								<h3>INQUIRY / FEEDBACK</h3><br />
+			
+				<table>
+					<?php if(isset($_POST['Submit'])) { ?>
+						<?php	
+							date_default_timezone_set('Asia/Calcutta');
+							$name = $_POST['name'];
+							$email = $_POST['email'];
+							$number = $_POST['num'];
+							$type = $_POST['type'];
+							$area = $_POST['area'];
+							$city = $_POST['city'];
+							$country = $_POST['country'];
+							$message = $_POST['message'];
+							$headers = "From: Tamanna@gmail.com\r\n" .  "Reply-To: " . $email;
+							$body = "\r\nName: " . $name . "\r\nEmail ID: " . $email ."\r\nContact Number:" . $number . "\r\nType: " . $type .  "\r\nArea: " . $area . "\r\nCity: " . $city . "\r\nCountry: " . $country . "\r\nMessage: " . $message ."\r\n\r\nSent:" . " " . date('l jS \of F Y h:i:s A');	
+							$to  = "tamannango@gmail.com"; // note the comma
+							mail($to, 'Tamanna Foundation Website Mail', $body, $headers);				
+						?>
+						<p class="confirm">Thank you for writing to us.<br/> We will get back to you as soon as possible.<br /><a href="index.html" style="text-decoration:none;color:#fff;">Take Me Back to Home</a></p><br /><br /><br /><br /><br /><br />
+					<?php } else { ?>															
+				<form action="?p=contact" method="post" id="contactform" name="contactform" >
+					<form id="contact" name="contact" action="#">
+			
+						<tr>
+							<td  valign="top">Name <span>*</span></td><td><input type="text" name="name" id="name" style="height:25px;width:257px;" value="" /></td>
+						</tr>
+				
+						<tr>
+							<td  valign="top">Email <span>*</span></td><td><input type="text" name="email" id="email" style="height:25px;width:257px;" value="" /></td>
+						</tr>
+				
+						<tr>
+							<td  valign="top">Number <span>*</span></td><td><input type="text" name="num" id="num" style="height:25px;width:257px;" value="" /></td>
+						</tr>
+						
+						<tr>
+							<td  valign="top">Type <span>*</span></td><td><select name="type" id="type" style="height:25px;width:262px;">
+													<option value="donor">Donor</option>
+													<option value="volunteer">Volunteer</option>
+													<option value="intern">Intern</option>
+													<option value="general">General</option>
+												</select>
+							</td>
+						</tr>
+						
+						<tr>
+							<td  valign="top">Area </td><td><input type="text" name="area" id="area" style="height:25px;width:257px;" value="" /></td>
+						</tr>
+				
+						<tr>
+							<td  valign="top">City <span>*</span></td><td><input type="text" name="city" id="city" style="height:25px;width:257px;" value="" /></td>
+						</tr>
+				
+						<tr>
+							<td  valign="top">Country <span>*</span></td><td><input type="text" name="country" id="country" style="height:25px;width:257px;" value="" /></td>
+						</tr>
+						
+						<tr>
+							<td  valign="top">Message <span>*</span></td><td style="padding-left:0px;"><textarea id="message" name="message" rows="5" cols="34"></textarea></td>
+						</tr>
+						
+						<tr>
+							<td></td>
+							<td height="50">
+								<input type="submit" name="Submit" value="Submit" >
+								<input type="reset" name="Reset" value="Reset" >
+							</td>
+						</tr>
+				
+					</form>
+			<?php } //endif ?>
+			</table>
+						</div>
+						
+						<div class="clear"></div>
+					</div>
+			
+			<div class="footerback">
+				<div class="footer" style="padding-top:35px;">
+					<div id="left">
+						&copy; 2013 Tamanna Foundation. All Rights Reserevd.
+					</div>
+					
+					<div id="middle">
+						Powered by <a href="http://www.overclockersdesign.com/" target="_blank" title="Overclockers Design">Overclockers Design</a>
+					</div>	
+					</div>
+				</div>
+			</div>	
+	</body>
+</html>	
